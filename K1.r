107a1911d4c9e37014c2bc60ce9e7c6841c94379
Justin Abraham <jrpabraham@gmail.com>
2017-07-13 15:09:09 -0700

    Working version of K1.r

--- K1.r
+++ K1.r
@@ -3,6 +3,7 @@
 ##############################
 
 setwd("/Users/Justin/Google Drive/UBIF/UBIF_Deliverables/UBIF_PAP/K1_PAP") # make this more flexible
+set.seed(47269801)
 
 required.packages <- c("dplyr", "lmtest", "sandwich", "stargazer", "ggplot2", "xtable", "pwr", "wesanderson", "extrafont", "multiwayvcov", "multcomp", "knitr")
 packages.missing <- required.packages[!required.packages %in% installed.packages()[,"Package"]]
@@ -16,9 +17,9 @@ lapply(required.packages, library, character.only = TRUE)
 
 ## RegTest tests primary hypotheses from linear model ##
 
-RegTest <- function(equation, clustvars, hypotheses) {
+RegTest <- function(equation, clustvars, hypotheses, data) {
 
-    model <- lm(equation)
+    model <- lm(equation, data = data)
 
     if (missing(clustvars)) model$vcov <- vcov(model)
     else model$vcov <- cluster.vcov(model, cluster = clustvars)
@@ -27,17 +28,18 @@ RegTest <- function(equation, clustvars, hypotheses) {
 
     numhyp <- length(hypotheses)
 
-    EST <- matrix(nrow = numhyp, ncol = 3)
+    EST <- matrix(nrow = numhyp, ncol = 4)
 
     for (i in 1:numhyp) {
 
         EST[i, 1] <- model$test$coefficients[i]
-        EST[i, 2] <- model$test$sigma[i]
-        EST[i, 3] <- model$test$pvalues[i]
+        EST[i, 2] <- model$test$tstat[i]
+        EST[i, 3] <- model$test$sigma[i]
+        EST[i, 4] <- model$test$pvalues[i]
 
     }
 
-    colnames(EST) <- c("Estimate", "SE", "P")
+    colnames(EST) <- c("Estimate", "Tstat", "SE", "P")
 
     return(EST)
 
@@ -45,28 +47,36 @@ RegTest <- function(equation, clustvars, hypotheses) {
 
 ## PermTest returns approximations of the exact p-value ##
 
-PermTest <- function(equation, clustvars, hypotheses) {
+PermTest <- function(equation, treatvars, clustvars, hypotheses, iterations, data) {
 
-    model <- lm(equation)
+    stopifnot(length(hypotheses) <= 1)
 
-    if (missing(clustvars)) model$vcov <- vcov(model)
-    else model$vcov <- cluster.vcov(model, cluster = clustvars)
+    obsEST <- RegTest(equation, clustvars, hypotheses, data)
+    obsStat <- obsEST[1, 2]
 
-    model$test <- summary(glht(model, linfct = hypotheses, vcov = model$vcov))$test
+    simEST <- matrix(ncol = 4)
 
-    numhyp <- length(hypotheses)
+    for (i in 1:iterations) {
 
-    EST <- matrix(nrow = numhyp, ncol = 3)
+        simTreat <- data[, treatvars, drop = FALSE]
+        simTreat <- simTreat[sample(nrow(simTreat)),]
 
-    for (i in 1:numhyp) {
+        simData <- cbind(simTreat, data[, !(names(data) %in% treatvars), drop = FALSE])
+        colnames(simData) <- c(treatvars, colnames(simData)[(1+length(treatvars)):ncol(simData)])
 
-        EST[i, 1] <- model$test$coefficients[i]
-        EST[i, 2] <- model$test$sigma[i]
-        EST[i, 3] <- model$test$pvalues[i]
+        simEST <- rbind(simEST, RegTest(equation, clustvars, hypotheses, data = simData))
 
     }
 
-    colnames(EST) <- c("Estimate", "SE", "P")
+    simSTAT <- simEST[2:nrow(simEST), 2]
+    countSTAT <- matrix(abs(simSTAT) >= abs(obsStat), ncol = 1)
+
+    ExactP <- matrix(1, nrow = 1, ncol = nrow(countSTAT)) %*% countSTAT
+    ExactP <- ExactP / iterations
+
+    EST <- cbind(obsEST, ExactP)
+
+    colnames(EST) <- c("Estimate", "Tstat", "SE", "P", "ExactP")
 
     return(EST)
 
@@ -80,40 +90,57 @@ FDR <- function(pvals) {
 
 }
 
-###############
-## Load data ##
-###############
-
-cars_data <- read_dta("../data/auto.dta")
-
-## Append all survey versions ##
-
 ################
 ## Clean data ##
 ################
+
+#Create locals for simulation
+  OBS <- 1000
+  TE  <- .8
+  HET <- .4
+
+#Generate treatment
+  Treat <- sample(0:1,OBS,rep = TRUE,prob = c(.5,.5)) %>%
+  factor(levels = c(0,1), labels = c("Control","Treatment"))
+
+#Generate gender
+ Gen <- sample(0:1,OBS,rep = TRUE,prob = c(.5,.5))  %>%
+  factor(levels = c(0,1), labels = c("Male","Female"))
+
+#Generate factor variable measuring highest level of education
+ Edu <- sample(1:3,OBS,rep = TRUE,prob = c(.5,.3,.2)) %>%
+  factor(levels = c(1,2,3), labels = c("Primary school","High school","University & above"))
+
+#Generate income
+ LnInc <- rnorm(OBS, mean = 5, sd = 1)
+ Inc <- exp(LnInc)
+
+#Generate y with notreatment effect
+  y_nottreat <- rnorm(OBS, 0, 1)
+
+#Generate outcome with noisy treatment effect of ___
+  y_Teffect <- rnorm(OBS, TE, 1)
+  y_Teffect[Treat == "Control"] <- 0
+  y_treated = y_nottreat + y_Teffect
+
+#Generate outcome with noisy treatment effect of ___ and noisy het of ___ gender
+  y_GenTeffect <- rnorm(OBS, HET, 1)
+  y_GenTeffect[Treat == "Control"] <- 0
+  y_GenTeffect[Gen == "Male"] <- 0
+  y_HetTreated <- y_treated + y_GenTeffect
+
+#Generate id
+  ID <- matrix(1:OBS, ncol = 1)
+
+#Create, save dataframe
+  TestData <- data.frame(ID, Treat, Gen, Edu, Inc, y_nottreat, y_treated, y_HetTreated)
 
 ################
 ## Estimation ##
 ################
 
-## Balance checks
+depvars <- c("y_nottreat", "y_treated", "y_HetTreated")
+hypotheses <- c("TreatTreatment = 0", "TreatTreatment = 1", "TreatTreatment = 0.8")
 
-## Primary hypotheses tests
-
-# Model1 <- lm(depvar ~ treatvars)
-# MainFx(Model1, clustvars = surveyid, hypotheses = c("Ind = 0", "Col = 0", "Ind - Col = 0"))
-
-## Covariate adjustment
-
-# Model1 <- lm(depvar ~ treatvars + controlvars)
-# MainFx(Model1, clustvars = surveyid, hypotheses = c("Ind = 0", "Col = 0", "Ind - Col = 0"))
-
-## Randomization inference
-
-# Model1 <- lm(depvar ~ treatvars)
-# PermFx(Model1, clustvars = surveyid, hypotheses = c("Ind = 0", "Col = 0", "Ind - Col = 0"))
-
-## Het effects
-
-# Model1 <- lm(depvar ~ treatvars*hetvar)
-# MainFx(Model1, clustvars = surveyid, hypotheses = c(""))
+RegTest(y_nottreat ~ Treat, clustvars = TestData$ID, hypotheses = c("TreatTreatment = 0"), data = TestData)
+PermTest(y_nottreat ~ Treat, treatvars = c("Treat"), clustvars = TestData$ID, hypotheses = c("TreatTreatment = 0"), iterations = 100, data = TestData)
