5f894146eae177b24ae1ed1514136c9a58257007
Justin Abraham <jrpabraham@gmail.com>
2017-07-13 17:25:00 -0700

    Knitr source code working.

--- K1.r
+++ K1.r
@@ -5,7 +5,7 @@
 setwd("/Users/Justin/Google Drive/UBIF/UBIF_Deliverables/UBIF_PAP/K1_PAP") # make this more flexible
 set.seed(47269801)
 
-required.packages <- c("dplyr", "lmtest", "sandwich", "stargazer", "ggplot2", "xtable", "pwr", "wesanderson", "extrafont", "multiwayvcov", "multcomp", "knitr")
+required.packages <- c("dplyr", "multiwayvcov", "multcomp", "knitr")
 packages.missing <- required.packages[!required.packages %in% installed.packages()[,"Package"]]
 
 if(length(packages.missing) > 0) {install.packages(required.packages, repo="https://cran.cnr.berkeley.edu/")}
@@ -15,11 +15,11 @@ lapply(required.packages, library, character.only = TRUE)
 ## Define functions ##
 ######################
 
-## RegTest tests primary hypotheses from linear model ##
+## RegTest conducts asymptotic test from linear model ##
 
 RegTest <- function(equation, clustvars, hypotheses, data) {
 
-    model <- lm(equation, data = data)
+    model <- lm(equation, data = data, na.action = na.omit)
 
     if (missing(clustvars)) model$vcov <- vcov(model)
     else model$vcov <- cluster.vcov(model, cluster = clustvars)
@@ -45,7 +45,7 @@ RegTest <- function(equation, clustvars, hypotheses, data) {
 
 }
 
-## PermTest returns approximations of the exact p-value ##
+## PermTest returns MC approximations of the exact p-value ##
 
 PermTest <- function(equation, treatvars, clustvars, hypotheses, iterations, data) {
 
@@ -62,7 +62,7 @@ PermTest <- function(equation, treatvars, clustvars, hypotheses, iterations, dat
         simTreat <- simTreat[sample(nrow(simTreat)),]
 
         simData <- cbind(simTreat, data[, !(names(data) %in% treatvars), drop = FALSE])
-        colnames(simData) <- c(treatvars, colnames(simData)[(1+length(treatvars)):ncol(simData)])
+        colnames(simData)[1:2] <- treatvars
 
         simEST <- rbind(simEST, RegTest(equation, clustvars, hypotheses, data = simData))
 
@@ -86,7 +86,7 @@ PermTest <- function(equation, treatvars, clustvars, hypotheses, iterations, dat
 
 FDR <- function(pvals) {
 
-    return pvals
+    return(pvals)
 
 }
 
@@ -94,53 +94,110 @@ FDR <- function(pvals) {
 ## Clean data ##
 ################
 
-#Create locals for simulation
-  OBS <- 1000
-  TE  <- .8
-  HET <- .4
+## Create locals for simulation ##
 
-#Generate treatment
-  Treat <- sample(0:1,OBS,rep = TRUE,prob = c(.5,.5)) %>%
-  factor(levels = c(0,1), labels = c("Control","Treatment"))
+  OBS <- 510
 
-#Generate gender
+## Generate treatment ##
+
+  Treat <- sample(0:2,OBS, rep = TRUE, prob = c(.33, .33, 0.33)) %>%
+  factor(levels = c(0, 1, 2), labels = c("Poverty", "Ind.", "Col."))
+
+  Pov <- (Treat == "Poverty") * 1
+  Ind <- (Treat == "Ind.") * 1
+  Col <- (Treat == "Col.") * 1
+
+## Generate gender ##
+
  Gen <- sample(0:1,OBS,rep = TRUE,prob = c(.5,.5))  %>%
   factor(levels = c(0,1), labels = c("Male","Female"))
 
-#Generate factor variable measuring highest level of education
+## Generate factor variable measuring highest level of education ##
+
  Edu <- sample(1:3,OBS,rep = TRUE,prob = c(.5,.3,.2)) %>%
   factor(levels = c(1,2,3), labels = c("Primary school","High school","University & above"))
 
-#Generate income
+## Generate income ##
+
  LnInc <- rnorm(OBS, mean = 5, sd = 1)
  Inc <- exp(LnInc)
 
-#Generate y with notreatment effect
-  y_nottreat <- rnorm(OBS, 0, 1)
+## Generate y with notreatment effect ##
 
-#Generate outcome with noisy treatment effect of ___
-  y_Teffect <- rnorm(OBS, TE, 1)
-  y_Teffect[Treat == "Control"] <- 0
-  y_treated = y_nottreat + y_Teffect
+  yNull <- rnorm(OBS, 0, 1)
 
-#Generate outcome with noisy treatment effect of ___ and noisy het of ___ gender
-  y_GenTeffect <- rnorm(OBS, HET, 1)
-  y_GenTeffect[Treat == "Control"] <- 0
-  y_GenTeffect[Gen == "Male"] <- 0
-  y_HetTreated <- y_treated + y_GenTeffect
+## Generate outcome with effects
+  yInd <- (0.8 * Ind) + rnorm(OBS, 0, 1)
+  yCol <- (0.4 * Col) + rnorm(OBS, 0, 1)
 
-#Generate id
+## Generate id ##
+
   ID <- matrix(1:OBS, ncol = 1)
 
-#Create, save dataframe
-  TestData <- data.frame(ID, Treat, Gen, Edu, Inc, y_nottreat, y_treated, y_HetTreated)
+## Create, save dataframe ##
+
+  TestData <- data.frame(ID, Treat, Pov, Ind, Col, Gen, Edu, Inc, yNull, yInd, yCol)
 
 ################
 ## Estimation ##
 ################
 
-depvars <- c("y_nottreat", "y_treated", "y_HetTreated")
-hypotheses <- c("TreatTreatment = 0", "TreatTreatment = 1", "TreatTreatment = 0.8")
+## Plain OLS ##
 
-RegTest(y_nottreat ~ Treat, clustvars = TestData$ID, hypotheses = c("TreatTreatment = 0"), data = TestData)
-PermTest(y_nottreat ~ Treat, treatvars = c("Treat"), clustvars = TestData$ID, hypotheses = c("TreatTreatment = 0"), iterations = 100, data = TestData)
+hypotheses <- c("Ind = 0", "Col = 1", "Ind - Col = 0")
+equations <- c("yNull ~ Ind + Col", "yInd ~ Ind + Col", "yCol ~ Ind + Col")
+
+for (h in hypotheses) {
+
+    RES <- matrix(nrow = 1, ncol = 5)
+
+    for (eqn in equations) {
+
+        # RES <- rbind(RES, RegTest(eqn, clustvars = TestData$ID, hypotheses = c(h), data = TestData))
+        RES <- rbind(RES, PermTest(eqn, treatvars = c("Treat"), clustvars = TestData$ID, hypotheses = c(h), iterations = 100, data = TestData))
+
+    }
+
+    RES <- RES[2:nrow(RES), 1:ncol(RES)]
+    RES <- cbind(RES, FDR(RES[, 4]))
+
+    rownames(RES) <- equations
+    colnames(RES)[6] <- "Min. Q"
+
+    print("--------------------------------------------------------------------", quote = FALSE)
+    print(paste("H_0:", h), quote = FALSE)
+    print(RES, quote = FALSE)
+    print("--------------------------------------------------------------------", quote = FALSE)
+
+}
+
+## Covariate adjustment ##
+
+hypotheses <- c("Ind = 0", "Col = 1", "Ind - Col = 0")
+equations <- c("yNull ~ Ind + Col + Gen + LnInc", "yInd ~ Ind + Col + Gen + LnInc", "yCol ~ Ind + Col + Gen + LnInc")
+
+for (h in hypotheses) {
+
+    RES <- matrix(nrow = 1, ncol = 5)
+
+    for (eqn in equations) {
+
+        # RES <- rbind(RES, RegTest(eqn, clustvars = TestData$ID, hypotheses = c(h), data = TestData))
+        RES <- rbind(RES, PermTest(eqn, treatvars = c("Treat"), clustvars = TestData$ID, hypotheses = c(h), iterations = 100, data = TestData))
+
+    }
+
+    RES <- RES[2:nrow(RES), 1:ncol(RES)]
+    RES <- cbind(RES, FDR(RES[, 4]))
+
+    rownames(RES) <- equations
+    colnames(RES)[6] <- "Min. Q"
+
+    print("--------------------------------------------------------------------", quote = FALSE)
+    print(paste("H_0:", h), quote = FALSE)
+    print(RES, quote = FALSE)
+    print("--------------------------------------------------------------------", quote = FALSE)
+
+}
+
+## Het effects ##
